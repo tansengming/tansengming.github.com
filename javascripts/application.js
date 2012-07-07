0d7c7803b849c4dd91f16dd603ef90cc71cda679
SengMing Tan <tansengming@gmail.com>
2012-07-07 11:31:53 +1000

    minor code changes

--- javascripts/application.js
+++ javascripts/application.js
@@ -21,14 +21,14 @@ g.src = ('https:' == location.protocol ? 'https://ssl' : 'http://www') + '.googl
 s.parentNode.insertBefore(g, s);
 })(document, 'script');
 
-// disqus
+// DISQUS
 (function() {
   var links = document.getElementsByTagName('a');
   var query = '?';
   for(var i = 0; i < links.length; i++) {
-  if(links[i].href.indexOf('#disqus_thread') >= 0) {
-    query += 'url' + i + '=' + encodeURIComponent(links[i].href) + '&';
-  }
+    if(links[i].href.indexOf('#disqus_thread') >= 0) {
+      query += 'url' + i + '=' + encodeURIComponent(links[i].href) + '&';
+    }
   }
   document.write('<script charset="utf-8" type="text/javascript" src="http://disqus.com/forums/simplest/get_num_replies.js' + query + '"></' + 'script>');
 })();
