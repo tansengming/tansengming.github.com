0e4736409e32a2dcea2dbbb1723c01839c29ecc7
SengMing Tan <tansengming@gmail.com>
2017-07-26 13:55:09 -0400

    remove disqus js

--- javascripts/application.js
+++ javascripts/application.js
@@ -20,15 +20,3 @@ g.async = true;
 g.src = ('https:' == location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
 s.parentNode.insertBefore(g, s);
 })(document, 'script');
-
-// DISQUS
-(function() {
-  var links = document.getElementsByTagName('a');
-  var query = '?';
-  for(var i = 0; i < links.length; i++) {
-    if(links[i].href.indexOf('#disqus_thread') >= 0) {
-      query += 'url' + i + '=' + encodeURIComponent(links[i].href) + '&';
-    }
-  }
-  document.write('<script charset="utf-8" type="text/javascript" src="//disqus.com/forums/simplest/get_num_replies.js' + query + '"></' + 'script>');
-})();
